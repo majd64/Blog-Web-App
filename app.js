7a19edd7f5ddfaee1b570a529ca099aa7a8c4551
Majd Hailat <majdhailat64@gmail.com>
2020-08-23 23:03:33 -0400

    add more

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
 
-let posts = [];
-function Post(title, body) {
+let posts = [
+  new Post("title of post", "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.", "There are many variations of passages of Lorem Ipsum available")
+];
+function Post(title, body, preview) {
     this.title = title
-    this.body = body;
+    this.body = body
+    this.preview = preview
 }
 
 app.get("/", function(req, res) {
@@ -36,8 +39,22 @@ app.get("/contact", function(req, res) {
   res.render("contact");
 });
 
-app.post("/compose", function(req, res){
-  const post = new Post(req.body.title, req.body.body);
+app.get("/post/:postName", function(req, res){
+  console.log(req.params.postName.replace(/-/g, ' '));
+  for (var i = 0; i < posts.length; i ++){
+    if (posts[i].title === req.params.postName.replace(/-/g, ' ')){
+      res.render("post", {
+        post: posts[i]
+      });
+    }
+
+  }
+});
+
+app.post("/compose", async function(req, res){
+  const post = new Post(req.body.title, req.body.body, req.body.body.slice(0, 100) +"...");
   posts.push(post);
   res.redirect("/");
 });
+
+app.post("/post")
